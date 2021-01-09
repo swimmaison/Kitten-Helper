d37f2248231539e49eac8c22d36919eaf740454f
Rebecca88 <rebecca88oliver@gmail.com>
2021-01-09 12:55:51 -0500

    updating front end
    Fixing components, added photo gallery. These will not be on the homepage, they are just there for development.

--- src/Components/KittenList.js
+++ src/Components/KittenList.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+class KittenList extends Component {
+    constructor () {
+        super()
+        this.state = {
+            message: 'Kitten 1'
+        }
+    }
+
+clickHandler(){
+
+this.setState({
+    message: 'kitten2'
+})
+}
 
 
-const KittenList = (props) =>{
-console.log(props)
-return <h1>
-   <article class="panel is-primary">
-    <div class="pannel-block is-active">
-<span class="panel-icon">
-<i class="fas fa-book"></i>
-Current Kitties:
-<button> 
-    {props.name}, {props.dob} months old
-        
-    </button>
-</span>
-</div>
 
-</article>
-</h1>
+    render() {
+        return (<div> 
+            <h1>
+                Kitten List:
+                {this.state.message}
+            </h1>
+            <button onClick = {this.clickHandler.bind(this)}>Add a Kitten</button>
+
+        </div>
+           
+        )
+    }
 }
 
 export default KittenList
