726eda20a8522e2d68fdd4e168e5df25637d7270
MichaelMayor <michael.davidge.mayor@gmail.com>
2021-01-10 12:59:49 -0500

    created table file

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,3 +1,24 @@
 function table(props){
-    
+    const header = Object.keys(props).map(item => <th>{item}</th>);
+
+ let tabData;
+for (let i = 0; i<props.dates.length; i++){
+    let entries
+ header.forEach((column) => {
+     entries.push(<td>{column[i]}</td>) 
+ })
+ tabData.push(<tr>{entries}</tr>)
 }
+
+return <table class="table">
+    <thead>
+        <tr>
+            {header}
+        </tr>
+    </thead>
+    <tbody>
+        {tabData}
+    </tbody>
+</table>
+};
+export default table
