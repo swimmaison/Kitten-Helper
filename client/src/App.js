898cae3471607aefe835e94286f87951d786d49a
MichaelMayor <michael.davidge.mayor@gmail.com>
2021-01-13 00:10:20 -0500

    I diiiiid it. Uninstalled excess React copy

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Weight from './Pages/Weight'
+import { requirePropFactory } from '@material-ui/core';
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
 
 function App(){
+  
   return (
     <div className="App">
       <header className="App-header">
