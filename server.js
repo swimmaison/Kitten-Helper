632d90dd24b08222ee73e44e23fb9b19e7f69fb3
MichaelMayor <michael.davidge.mayor@gmail.com>
2021-01-23 12:46:13 -0500

    Mongo env variable

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kittenhelper')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kittenhelper',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Add routes, both API and view
 app.use(routes)
