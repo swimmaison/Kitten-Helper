6443f984407e4eb5ad6d55b8cc7232e01e91d0df
MichaelMayor <michael.davidge.mayor@gmail.com>
2021-01-25 16:33:24 -0500

    Photo page kinda working

---
+++ client/src/Components/ImgurEmbed/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import useScript from '../../utils/useScript'
+export default function ImgurEmbed (props) {
+  useScript('http://s.imgur.com/min/embed.js')
+  return <blockquote className="imgur-embed-pub" lang="en" data-id={props.data_id} ><a href={props.src}>Found a kitten</a></blockquote>
+}
--- client/src/Pages/PhotoGallery.js
+++ client/src/Pages/PhotoGallery.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import ImgurEmbed from '../Components/ImgurEmbed'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,23 +25,23 @@ export default function PhotoGallery () {
           <Grid container spacing={3}>
               <Grid item xs>
                   <Paper className={classes.paper}>
-                      <blockquote className="imgur-embed-pub" lang="en" data-id="Qa1a2ef"><a href="//imgur.com/Qa1a2ef">Found a kitten</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+                      <ImgurEmbed data_id="Qa1a2ef" src="http://imgur.com/Qa1a2ef"/>
                   </Paper>
               </Grid>
               <Grid item xs>
                   <Paper className={classes.paper}>
-                      <blockquote className="imgur-embed-pub" lang="en" data-id="KWvtdg0"><a href="//imgur.com/KWvtdg0">&quot;As Gentle as a Cat Touching a Cherry&quot;</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+                      <ImgurEmbed data_id="KWvtdg0" src="http://imgur.com/KWvtdg0" />
                   </Paper>
               </Grid>
               <Grid item xs>
                   <Paper className={classes.paper}>
-                      <blockquote className="imgur-embed-pub" lang="en" data-id="sRLWmUh"><a href="//imgur.com/sRLWmUh">Mistakes were made</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>                  </Paper>
+                      <ImgurEmbed data_id="sRLWmUh" src="http://imgur.com/sRLWmUh" />               </Paper>
               </Grid>
           </Grid>
           <Grid container spacing={3}>
               <Grid item xs>
                   <Paper className={classes.paper}>
-                      <blockquote className="imgur-embed-pub" lang="en" data-id="uq7Ssi0"><a href="//imgur.com/uq7Ssi0">60fps Kitten Cuddle</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>                  </Paper>
+                      <ImgurEmbed data_id="uq7Ssi0" src="http://imgur.com/uq7Ssi0" />                  </Paper>
               </Grid>
           </Grid>
       </div>
